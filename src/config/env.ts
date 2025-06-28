5fcf7a7abcc1b12f1f293b88d37e83b4d76f51b3
AmirAkbarpour1 <amirakbarpour86@gmail.com>
2025-06-29 01:13:48 +0330

    Migrate node env to Deno env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,19 +1,18 @@
 import * as v from 'valibot';
 import 'dotenv/load';
-import process from 'node:process';
 
 const envSchema = v.object({
   TELEGRAM_BOT_TOKEN: v.pipe(v.string(), v.minLength(1)),
 });
 
-const envParsed = v.safeParse(envSchema, process.env);
+const envParsed = v.safeParse(envSchema, Deno.env.toObject());
 
 if (!envParsed.success) {
   console.error(
     '❌ Invalid environment variables:',
     v.flatten(envParsed.issues),
   );
-  process.exit(1);
+  Deno.exit(1);
 }
 
 const env: v.InferOutput<typeof envSchema> = envParsed.output;
